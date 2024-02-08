e4e0a7b8bd771056f09a01ced606e09942092b61
unknown <abiodunolayinka75@gmail.com>
2024-02-08 13:48:50 +0100

    authentication not completed

--- src/api/authentication/auth.js
+++ src/api/authentication/auth.js
@@ -4,7 +4,9 @@ import {authKit} from './base'
 export const SignupAction = (data) => {
     return authKit.post('api/v1/user/signup', data)
 }
-
+export const LoginAction = (data) => {
+    return authKit.post('api/v1/user/login', data)
+}
 
 
 
