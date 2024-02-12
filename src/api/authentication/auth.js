be5b1ac1ff9f0f35fb03f126236d4b7d2937e964
Obidairo Adewale <mascot4sure@gmail.com>
2024-02-12 08:26:59 +0100

    signup feature implemented and working fine

--- src/api/authentication/auth.js
+++ src/api/authentication/auth.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 import {authKit} from './base'
 
-export const SignupAction = (data) => {
+export const signupAction = (data) => {
     return authKit.post('api/v1/user/signup', data)
+}
+export const googleLogin = (data) => {
+    return authKit.get('auth/google')
 }
 
 
