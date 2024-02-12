dfb2f43b90782d10587b8d870048ca3aaf0cd0b3
unknown <abiodunolayinka75@gmail.com>
2024-02-12 17:08:43 +0100

    waitlist

--- src/app/page.js
+++ src/app/page.js
@@ -179,25 +179,10 @@ const WaitList = () => {
       },
     });
   const closeModal = () => {
+    setShowModal(false);
     router.push("/");
   };
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 3, // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2, // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1, // optional, default to 1.
-    },
-  };
+  
   var settings = {
     dots: true,
     infinite: true,
@@ -205,7 +190,7 @@ const WaitList = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true, 
-    autoplaySpeed: 1000, 
+    autoplaySpeed: 1500, 
     responsive: [
       {
         breakpoint: 1024, // for screens larger than 1024px wide
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Modal = ({modalClose}) => {
   return (
     <section className=' relative'>
-        <div className='bg-[black] opacity-[60%] h-screen w-screen z-[1000]' onClick={modalClose}></div>
+        <div className='bg-[black] opacity-[60%] h-screen w-100% z-[1000]' onClick={modalClose}></div>
         <div className='w-[720px] lg:w-[600px] md:w-[85%] h-[436px] sm:h-[400px] mx-auto bg-[#fff] rounded-[24px] border-solid border-[1px] border-[#fff] z-[9900] absolute top-[calc(20%)] xl:top-[calc(30%)] lg:top-[calc(20%)] md:top-[calc(20%)] sm:top-[calc(15%)] left-[calc(25%)] xl:left-[calc(17%)] lg:left-[calc(14%)] md:left-[calc(14%)] sm:left-[calc(10%)]'>
             <Image src='/confirm.png' width={120} height={120} alt='confirm' className='mx-auto pt-[98.5px] sm:pt-[50px] pb-[10px]' />
             <h4 className='font-inter font-bold text-[32px] lg:text-[30px] md:text-[28px] leading-[40.83px] lg:leading-[38px] md:leading-[36px] text-[black] text-center'>Registration Confirmed</h4>
