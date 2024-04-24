2cd80ae951c88a925bc0e73570fde02cc11d6f66
abiodunolayinka <abiodunolayinka75@gmail.com>
2024-04-24 17:27:15 +0100

    implementation of mobile dropdown

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -1,49 +1,68 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Classes from "./nav.module.css";
 import Image from "next/image";
+import { useRouter, usePathname } from "next/navigation";
 
 const Navbar = () => {
   const [dropdown, setDropdown] = useState(false);
+  const [isActive, setIsActive] = useState("");
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const setActive = () => {
+    useEffect((pathname) => {
+      
+      setIsActive(pathname);
+    }, []);
+
+
+  }
+  console.log(pathname);
+  console.log(isActive);
 
   return (
     <>
       <header className={Classes.header} suppressHydrationWarning>
         <div className={Classes.navbar}>
-          <Link href="/" className={Classes.logo}>
+          <Link href="/" className={Classes.logo} >
             <Image src="/navLogo.svg" alt="logo" width={164} height={36} />
           </Link>
           <nav className={Classes.nav}>
             <ul>
               <li>
-                <Link href="/" onClick={() => setDropdown(false)}>
+                <Link href="/" className={isActive === "/" ? "active" : ""} onClick={setActive}>
                   Home
                 </Link>
               </li>
               <li>
-                <Link href="/about-us" onClick={() => setDropdown(false)}>
+                <Link
+                  href="/about-us"
+                  className={isActive === "/about-us" ? "active" : ""} onClick={setActive}
+                >
                   About Us
                 </Link>
               </li>
               <li>
-                <Link href="/faq" onClick={() => setDropdown(false)}>
-                  FAQ
-                </Link>
+                <Link href="/faq">FAQ</Link>
               </li>
               <li>
-                  <Link href="/donation" onClick={() => setDropdown(false)}>
-                    Donation
-                  </Link>
-                </li>
+                <Link href="/donation">Donation</Link>
+              </li>
             </ul>
             <div className={Classes.btnFlex}>
               <button>
-                <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/login">Log in</Link>
+                <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/login">
+                  Log in
+                </Link>
               </button>
               <button>
-                <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/signup">Sign up</Link>
+                <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/signup">
+                  Sign up
+                </Link>
               </button>
             </div>
           </nav>
@@ -72,13 +91,17 @@ const Navbar = () => {
                 </li>
               </ul>
               <div className={Classes.btnFlex}>
-              <button>
-                <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/login">Log in</Link>
-              </button>
-              <button>
-                <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/signup">Sign up</Link>
-              </button>
-            </div>
+                <button>
+                  <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/login">
+                    Log in
+                  </Link>
+                </button>
+                <button>
+                  <Link href="https://hackthejobs-web-dashoard-production.up.railway.app/auth/signup">
+                    Sign up
+                  </Link>
+                </button>
+              </div>
             </nav>
           ) : null}
           {!dropdown ? (
@@ -90,7 +113,7 @@ const Navbar = () => {
               className={Classes.dropdown}
               onClick={() => setDropdown(false)}
             >
-              <span style={{fontSize:"34px",  }}>&#215;</span>
+              <span style={{ fontSize: "34px" }}>&#215;</span>
             </div>
           )}
         </div>
