54f37a001dc9185139eaa49d6bca20f975477204
abiodunolayinka <abiodunolayinka75@gmail.com>
2024-09-03 09:34:50 +0100

    routing to ures to waitlist

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -42,7 +42,9 @@ const Signup = () => {
               We are happy to have you back. Select your account type
             </p>
             <div>
-                <a href="https://dashboard.hackthejobs.com/auth/signup" target="_blank">
+                {/* <a href="https://dashboard.hackthejobs.com/auth/signup" target="_blank"> */}
+                <a href="https://waitlist.hackthejobs.com" target="_blank">
+
                     <div style={{background: logo1 ? '#1453FF': '', color: logo1 ? '#fff' : ''}} className="bg-[#fff] px-[48px] py-[29px] mb-[28px] rounded-[8px] cursor-pointer border-[1px]  border-[#EAEAEA] flex justify-center items-center gap-[12px]" onMouseOver={handleLogoEvent1} onMouseOut={handleLogoEvent1}>
                         {logo1 ? <Image src='/hover-fellow.png' width={20} height={20} alt="fellow icon" /> : <Image src='/fellow.png' width={20} height={20} alt="fellow icon" />}
                         <p style={{ color: logo1 ? '#fff' : ''}} className="font-medium text-[16px] text-[#2A2A2A] leading-[24px] tracking-[3%]">Become a Fellow</p>
