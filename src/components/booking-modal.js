b9f9226927da11c7200cc3fa560e64c05d097a98
abioduolayinka <abiodunolayinka75@gmail.com>
2025-03-31 13:22:58 +0100

    bookings

--- src/components/booking-modal.js
+++ src/components/booking-modal.js
@@ -1,35 +1,10 @@
 
-// import Image from 'next/image';
-// import React, { useEffect, useState } from 'react';
-
-// const BookSession = ({ closeModal, mentorId, mentorImage }) => {
-//   const [bookingData, setBookingData] = useState({});
-//   const [loading, setLoading] = useState(false);
-
-//   return (
-//     <div>
-//       <div className='font-whyte'>
-//         <div
-//           className="bg-[#344054B2] opacity-[0.7] w-[100%] h-full fixed top-0 left-[0]"
-//           onClick={closeModal}
-//         ></div>
-//         <div className="bg-[#fff] max-w-[442px] md:max-w-full p-8 sm:p-3 pb-[277px] sm:pb-[41px] flex flex-col fixed z-50">
-//             <Image src='/booking-successful.png' width={57} height={57} alt='success' />
-//             <h3 className='font-medium font-bold text-[24px] text-[#121927] leading-[11.71px] py-[16px]'>Session Booked</h3>
-//             <p className='font-regular text-[16px] text-[#555555] leading-[24px]'>You've successfully booked a {15}-minute session with {'John Foe'} on {'Monday, August 7th, 2024'}, from {'3:00 PM to 3:15 PM'}.</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default BookSession;
 "use client"
 
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
-const BookingModal = ({ successModal, mentor }) => {
+const BookingModal = ({ closeModal, mentor }) => {
   const [bookingData, setBookingData] = useState({});
   const [loading, setLoading] = useState(false);
 
@@ -39,7 +14,7 @@ const BookingModal = ({ successModal, mentor }) => {
     <div className="relative">
       <div
         className="fixed inset-0 bg-[#344054B2] opacity-[0.7] z-40"
-        onClick={successModal}
+        onClick={closeModal}
       ></div>
       <div className="fixed inset-0 flex items-center justify-center z-50 p-4 sm:p-6 md:p-8">
         <div className="bg-[#fff] w-[447px] h-[291px]  md:max-w-full p-8 sm:p-6 pb-[277px] sm:pb-[41px] flex flex-col items-center text-center rounded-[8px]">
