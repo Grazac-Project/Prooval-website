aa7e05e9dcea9d648e6004d1f177643b3bb614ac
Obidairo Adewale <mascot4sure@gmail.com>
2025-04-29 16:45:41 +0100

    google analytics implemented

--- src/components/googleAnalytics.js
+++ src/components/googleAnalytics.js
@@ -1,21 +1,29 @@
+'use client'
 import Script from 'next/script';
+import { GA_ID } from '@/lib/gtag';
 
-const GoogleAnalytics = ({ GA_TRACKING_ID }) => {
+ const GoogleAnalytics = () => {
+  if (!GA_ID) return null;
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
         strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${GA_TRACKING_ID}');
-        `}
-      </Script>
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </>
   );
 };
