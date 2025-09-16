bdd62dd53914dfe326eaeca1b2f1ea9044a9b667
abiodunolayinka <abiodunolayinka75@gmail.com>
2025-09-16 17:46:09 +0100

    refactor: remove unused imports and clean up BookSession component

--- src/components/book-session.js
+++ src/components/book-session.js
@@ -296,32 +296,6 @@ const BookSession = ({
             </button>
           </div>
         ) : (
-
-      <div className="font-whyte ">
-        <div
-          className="bg-[#344054] opacity-[0.7] w-[100%] h-[100vh] fixed z-50 top-0 left-[0]"
-          onClick={closeModal}
-        ></div>
-
-        {isSuccess ? (
-          <div className="bg-[#fff] w-[447px] h-[291px]  md:max-w-full p-8 sm:p-6 pb-[277px] sm:pb-[41px] flex flex-col items-center text-center rounded-[8px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
-            <Image src="/sucess.svg" width={57} height={57} alt="success" />
-            <h3 className="font-medium  text-[24px] text-[#121927] leading-[11.71px] py-[16px]">
-              Session Booked
-            </h3>
-            <p className="font-regular text-[16px] text-[#555555] leading-[24px] mb-[20px]">
-              Your booking with {mentor?.mentor?.firstName}{" "}
-              {mentor?.mentor?.lastName} was successful. A confirmation email
-              has been sent to your inbox.
-            </p>
-            <button
-              className="w-[76px] h-[44px] rounded-[8px] border-[1px] px-[20px] py-[12px] font-medium bg-[#1453FF] text-[14px] text-[#fff] leading-[19.6px] tracking-[2%] mx-auto"
-              onClick={closeModal}
-            >
-              Done
-            </button>
-          </div>
-        ) : (
           <div className="h-full bg-[#fff] w-full max-w-[629px] md:max-w-full p-8 sm:p-3 pb-[277px] sm:pb-[41px] overflow-y-auto flex flex-col fixed top-0 right-0 z-50">
             <div className="pb-[40px] flex gap-[16px] items-center">
               <div
